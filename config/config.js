b830fb9ac3d12359bf5c5fd35ae27bc29440e220
Jamshed Rabadi <jamshedrabadi@gmail.com>
2026-06-13 00:12:20 +0530

    comments to help with config arrays

--- config/config.js
+++ config/config.js
@@ -1,13 +1,13 @@
 const config = {
-	ROOT_PATH: "",
+	ROOT_PATH: "", // root path of the project, use \\
 
-	ROOT_FILES: [],
+	ROOT_FILES: [], // files directly in the root folder to extract code from
 
-	ROOT_FOLDERS: [],
+	ROOT_FOLDERS: [], // folders in the root folder to extract code from
 
-	INCLUDED_EXTENSIONS: [],
+	INCLUDED_EXTENSIONS: [], // file extensions to include, leave empty to include all extensions
 
-	IGNORED_NAMES: [],
+	IGNORED_NAMES: [], // file/folder names to ignore, leave empty to include all names
 
 	OUTPUT_DIRECTORY: "output",
 
